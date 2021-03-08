98d35c08674634fa99582bc5261fd7c72d0cc5c6
FernandaThiesen <fcthiesen@gmail.com>
2021-03-07 23:17:04 -0500

    finished styles and added media queries

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import './App.scss';
+import { FaRegThumbsUp } from 'react-icons/fa';
 import Header from './Header.js';
 import Footer from './Footer.js';
 import Movie from './Movie.js';
 import firebase from './firebase.js';
 import { useState, useEffect } from 'react';
-
+import { confirmAlert } from 'react-confirm-alert'; // Import
+import 'react-confirm-alert/src/react-confirm-alert.css' // Import css
+import { AiOutlineLoading } from 'react-icons/ai';
 
 function App() {
 
   const [ movies, setMovies ] = useState([]);
-  const [ formFields, setFormFields ] = useState({ title: "", comment: "", where: ""});
+  const [ formFields, setFormFields ] = useState({ title: "", comment: "", where: ""})
+  const [ isLoading, setIsLoading ] = useState(true);;
 
  
   useEffect(() => {
 
-  const dbRef = firebase.database().ref();
+  const dbRef = firebase.database().ref('movies');
 
   dbRef.on('value', (data) => {
   
     // console.log(data.val().movies);
-    const movieData = data.val().movies;
+    const movieData = data.val();
     const movies = [];
     for ( let movieKey in movieData ) {
       movies.push({
@@ -31,7 +35,8 @@ function App() {
     }
     // console.log(movies);
     // console.log(data.val());
-    setMovies(movies);
+    setMovies(movies.reverse());
+    setIsLoading(false);
      
   })
 
@@ -44,8 +49,6 @@ const handleChange = (event) => {
     ...formFields,
     [event.target.name]: event.target.value
   });
-  // console.log(formFields);
-
 }
 
 const handleSubmit = (event) => {
@@ -56,7 +59,15 @@ const handleSubmit = (event) => {
     moviesRef.push(formFields);
     setFormFields({ title: "", comment: "", where: ""});
   } else {
-    alert('Nope');
+    confirmAlert({
+      title: 'Ooops !!',
+      message: 'Please fill out all the fields',
+      buttons: [
+        {
+          label: 'Gotcha!'
+        }
+      ]
+    })
   }
 }
 
@@ -64,7 +75,8 @@ const handleSubmit = (event) => {
     <div className="App wrapper container">
       <Header />
       <main>
-        <section className="form">
+      <section className="form">
+          <h2>Recommend</h2>
           <p>Use the form below to add a must-watch movie to the board.</p>
           <form action="" onSubmit={handleSubmit}>
             <label htmlFor="title" className="sr-only">Movie Title</label>
@@ -73,15 +85,19 @@ const handleSubmit = (event) => {
             <textarea id="comment" name="comment" cols="30" rows="10" maxLength="500" placeholder="Why is this movie a must-watch?" onChange={handleChange} value={formFields.comment}></textarea>
             <label htmlFor="where" className="sr-only">Where can it be watched?</label>
             <input type="text" id="where" name="where" placeholder="Where can it be watched?" onChange={handleChange} value={formFields.where}/>
-            <button>Recommend</button>
+            <button>Recommend <FaRegThumbsUp /></button>
           </form>
         </section>
+        {/* end of section form */}
         <section className="movies">
         {
-        movies.map((movie) => {
+          isLoading
+          ? <span className="loading"><AiOutlineLoading /></span>
+          : movies.map((movie) => {
           return (
             <Movie 
             key={movie.uniqueKey}
+            id={movie.uniqueKey}
             title={movie.title}
             comment={movie.comment}
             where={movie.where}
--- src/Movie.js
+++ src/Movie.js
@@ -1,8 +1,37 @@
+import firebase from './firebase.js';
+import { GrClose } from 'react-icons/gr';
+import { confirmAlert } from 'react-confirm-alert'; // Import
+import 'react-confirm-alert/src/react-confirm-alert.css' // Import css
+
 const Movie = (props) => {
+  
+  const handleClick = (uniqueId) => {
+
+    confirmAlert({
+      title: 'Are you sure...',
+      message: '...you want to delete this recommendation?',
+      buttons: [
+        {
+          label: 'Yes',
+          onClick: () => {
+            const moviesRef = firebase.database().ref('movies');
+            moviesRef.child(uniqueId).remove();
+          }
+        },
+        {
+          label: 'No'
+        }
+      ]
+    })
+
+
+  }
   return (
     <div className="movie">
+      <button onClick={ () => {handleClick(props.id)}} className="close" aria-label="Remove recommendation" title="Remove recommendation"><GrClose /></button>
       <h2>{props.title}</h2>
       <p>{props.comment}</p>
+      <h4>Where to watch it:</h4>
       <p>{props.where}</p>
     </div>
   )
